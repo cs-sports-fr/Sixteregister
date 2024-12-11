7a0d5fcf5b95f99b7a32d111cbe1e4c46ea06b17
Aliane Sebastien <sebastien.aliane@student-cs.fr>
2024-12-12 00:54:36 +0100

    okkk

--- frontend/src/pages/vitrine/Home.jsx
+++ frontend/src/pages/vitrine/Home.jsx
@@ -1,7 +1,7 @@
 import { Box, Button, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper,Fade,Slide } from "@mui/material";
 import LayoutUnauthenticated from "../../components/layouts/LayoutUnauthenticated";
 import Apropos from "../../components/Home/apropos";
-
+import Deroulementjournee from "../../components/Home/deroulementjournee";
 const Home = () => {
     const isDarkMode = false;
 
@@ -11,6 +11,8 @@ const Home = () => {
         <LayoutUnauthenticated isDarkMode={isDarkMode}>
             <Box>
                 <Apropos/>
+                <Deroulementjournee/>
+
             </Box>
         </LayoutUnauthenticated>
     );
